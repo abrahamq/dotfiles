719f37614a030676ea3d6401d8e6a4d52dff46c1
Abraham Quintero <abrahamq@mit.edu>
2015-03-15 15:11:29 -0400

    removed .machineSpecific.sh

--- .machineSpecific.sh
+++ .machineSpecific.sh
@@ -1,5 +1,6 @@
-#PATH="/root/.gem/ruby/2.2.0/bin:$PATH"
-PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
+#export for PIN tool make 
+export PIN_HOME=/home/abrahamq/Documents/classes/Spring15/6.823/classPin
+export PIN_ROOT=$PIN_HOME
+export PIN_KIT=$PIN_HOME
+export PATH=$PIN_HOME:$PATH
 
-PATH=$PATH:~/.node_modules/bin
-export npm_config_prefix=~/.node_modules
